0525ea08acc66ba5aba3cb8c23d297d434f657e5
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-11-30 21:38:37 -0500

    initial commit

---
+++ src/main/java/espol/designpatternsone/Designpatternsone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espol.designpatternsone;
+
+/**
+ *
+ * @author braulio04
+ */
+public class Designpatternsone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
